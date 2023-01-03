c954be04b4d33b8ca45b1e08bcb818c534b1f174
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-01-03 09:24:28 -0700

    Add interests

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -13,4 +13,17 @@ RSpec.describe Patron do
             expect(patron_1.spending_money).to eq(20)
         end
     end
+
+    describe "#interests" do
+        it "has no interests by default" do
+            expect(patron_1.interests).to eq([])
+        end
+
+        it "can have interests" do
+            patron_1.add_interest("Dead Sea Scrolls")
+            patron_1.add_interest("Gems and Minerals")
+
+            expect(patron_1.interests).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
+        end
+    end
 end
