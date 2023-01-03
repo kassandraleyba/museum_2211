fd4d92f71330715df40ed08f63048fc548bbc17c
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-01-03 09:14:55 -0700

    Add initialize

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -2,5 +2,16 @@ require 'rspec'
 require './lib/exhibit'
 
 RSpec.describe Exhibit do
-  
+  let(:exhibit)  { Exhibit.new({name: "Gems and Minerals", cost: 0}) }
+
+  describe "#initialize" do
+    it "exists" do
+        expect(exhibit).to be_instance_of(Exhibit)
+    end
+
+    it "has attributes" do
+        expect(exhibit.name).to eq("Gems and Minerals")
+        expect(exhibit.cost).to eq(0)
+    end
+  end
 end
